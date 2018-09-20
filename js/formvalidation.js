8b3bd509002be0d52795dd04f9e30272fc57b999
Iliyan Stoyanov <djrfung@outlook.com>
2018-09-19 19:40:05 -0700

    Pushed old work

---
+++ js/formvalidation.js
@@ -0,0 +1,4 @@
+// this document is for validating the form and writing the results to the local storage
+function errorMessage() {
+  console.log("The TBA API is either offline or there is no available match schedule for the given event, so we need to manually input the match schedule");
+};
---
+++ js/index.js
@@ -0,0 +1,3 @@
+// add a part where if the match schedule cannot be pulled from TBA, give an option to put it in manually, in a form embedded in an iframe
+
+// import
