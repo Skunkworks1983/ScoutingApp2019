fff6fd479fdfd05d306f0920870257ed8e41e05e
Iliyan Stoyanov <djrfung@outlook.com>
2018-09-27 22:20:12 -0700

    Added View Match Schedule Functionality
    it might not be in the TBA format however

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,18 +1,16 @@
-
-while(false) {
-const eventcode = document.getElementById("eventcode");
-const matchno = document.getElementById("matchno")
-// red ids
-const red1 = document.getElementById("red1");
-const red2 = document.getElementById("red2");
-const red3 = document.getElementById("red3");
-// blue ids
-const blue1 = document.getElementById("blue1");
-const blue2 = document.getElementById("blue2");
-const blue3 = document.getElementById("blue3");
-// make sure this loop doesn't run too much
-
-}
+// while(false) {
+// const eventcode = document.getElementById("eventcode");
+// const matchno = document.getElementById("matchno")
+// // red ids
+// const red1 = document.getElementById("red1");
+// const red2 = document.getElementById("red2");
+// const red3 = document.getElementById("red3");
+// // blue ids
+// const blue1 = document.getElementById("blue1");
+// const blue2 = document.getElementById("blue2");
+// const blue3 = document.getElementById("blue3");
+// // make sure this loop doesn't run too much
+// }
 
 // this document is for validating the form and writing the results to the local storage
 function errorMessage() {
@@ -44,7 +42,16 @@ function errorMessage() {
 // for some reason Patrick was still using the v2 API
 
 // test JSON objects
-testObj = {"eventname":"2018wapp", "match":[1,2], "red1":[254,6969], "red2":[118,420], "red3":[1983,7005], "blue1":[168,999], "blue2":[7005,666], "blue3":[5803,69]}
+testObj = {
+            "eventname":"2018wapp",
+            "match":[1,2,3,4],
+            "red1":[254,6969,4556,443],
+            "red2":[118,420,809,3122],
+            "red3":[1983,7005,9301,837],
+            "blue1":[168,999,3293,7463],
+            "blue2":[7005,666,321,8402],
+            "blue3":[5803,69,6283,971]
+          }
 
 // for the final version of the JSON object we should nest multiple objects inside of each other, so that we can store all events in a single JSON object to simplify the code necessary for the implementation of viewSchedule
 
@@ -92,7 +99,7 @@ function submitSchedule() {
     case blue3 <= 9999 && blue3 >= 1:
       checksum++;
       console.log(checksum);
-      
+
     // default:
     //   return true;
   }
@@ -102,5 +109,59 @@ function submitSchedule() {
     return true;
   } else {
     return false;
+  }
+}
+
+// function to add rows to the table
+function addRows() { // event nombre is a tribute to Patrick and his wonderful code
+  var i = 0;
+  var eventNombre = testObj.eventname;
+  // matchno = document.createTextNode(testObj.match);
+  for(i=0; i < (testObj.match.length); i++) {
+    if (!document.getElementsByTagName) return;
+    // find the table
+    tabBody = document.getElementsByTagName("tbody").item(0);
+
+    // create a new row
+    row = document.createElement("tr");
+
+    // how many cells are included
+    cell1 = document.createElement("td");
+    cell2 = document.createElement("td");
+    cell3 = document.createElement("td");
+    cell4 = document.createElement("td");
+    cell5 = document.createElement("td");
+    cell6 = document.createElement("td");
+    cell7 = document.createElement("td");
+
+    // text nodes
+    matchno = document.createTextNode(testObj.match[i]);
+    red1 = document.createTextNode(testObj.red1[i]);
+    red2 = document.createTextNode(testObj.red2[i]);
+    red3 = document.createTextNode(testObj.red3[i]);
+    blue1 = document.createTextNode(testObj.blue1[i]);
+    blue2 = document.createTextNode(testObj.blue2[i]);
+    blue3 = document.createTextNode(testObj.blue3[i]);
+
+    // add text values to the cells
+    cell1.appendChild(matchno);
+    cell2.appendChild(red1);
+    cell3.appendChild(red2);
+    cell4.appendChild(red3);
+    cell5.appendChild(blue1);
+    cell6.appendChild(blue2);
+    cell7.appendChild(blue3);
+
+    // add td to try
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    row.appendChild(cell4);
+    row.appendChild(cell5);
+    row.appendChild(cell6);
+    row.appendChild(cell7);
+
+    // add tr to the table body
+    tabBody.appendChild(row);
   }
 }
