3b1253d6b450a1a45d195bd091e1b581c8c43dfc
Iliyan Stoyanov <djrfung@outlook.com>
2018-10-21 13:48:10 -0700

    Table Creation (for meeting tool) is done

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,51 +1,65 @@
+// viewSchedule.html
+// define the target url
+let eventNombre;
+
+// tba key in array form
+const TBAkey = ["X-TBA-Auth-Key", "d4V33bAbuXiKfuLW1pc4BaLbr56BgiORtyM5hwmRLU5qNf6Rxh83noDdI0mPJJ3R"];
+
+// define the base url
+const TBAurl = "https://www.thebluealliance.com/api/v3/event/";
+
+// http request
+const eventtarget = new XMLHttpRequest();
+let scheduleObj;
 
 function TBArequest() {
-  let eventNombre = document.getElementById('EventNombre').value;
-  const TBAkey = "?X-TBA-Auth-Key=d4V33bAbuXiKfuLW1pc4BaLbr56BgiORtyM5hwmRLU5qNf6Rxh83noDdI0mPJJ3R";
-  const TBAurl = "https://www.thebluealliance.com/api/v3/event/";
-  const Http = new XMLHttpRequest();
-  Http.open("GET", TBAurl.concat(eventNombre + "/matches"));
-  Http.setRequestHeader("X-TBA-Auth-Key", "d4V33bAbuXiKfuLW1pc4BaLbr56BgiORtyM5hwmRLU5qNf6Rxh83noDdI0mPJJ3R");
-  Http.send();
+  eventNombre = document.getElementById('EventNombre').value;
+  console.log("Target Event is " + eventNombre);
+  eventtarget.open("GET", TBAurl.concat(eventNombre + "/matches"));
+  eventtarget.setRequestHeader(TBAkey[0], TBAkey[1]);
+  eventtarget.send();
+  console.log("Request Sent");
+  changeButtonColor("startbutton", "#fffa9b");
 
-  Http.onreadystatechange = function() {
-    if(this.readyState===4 && this.status==200) {
-      // var TBAresponse = Http.parse();
-      console.log(Http.responseText);
+  eventtarget.onreadystatechange = function() {
+    if(this.readyState===4) {
+      switch(this.status) {
+          case 200:
+            console.log("Schedule request completed successfully");
+            scheduleObj = JSON.parse(eventtarget.responseText);
+            changeButtonColor("startbutton", "#b7ffb4");
+            break;
+
+          case 401:
+            console.warn("TBA API key is invalid");
+            console.info("Enter a valid key");
+            changeButtonColor("startbutton", "#ffb5b5");
+            alert("TBA API key is invalid; Enter a valid key por favor. Error 401 on schedule <-- for the tech support");
+            break;
+
+          case 404:
+            console.info("Invalid URL");
+            changeButtonColor("startbutton", "#ffb5b5");
+            alert("Invalid URL entered. Error 404 on schedule <-- for the tech support");
+            break;
+
+          default:
+            changeButtonColor("startbutton", "#ffb5b5");
+            console.warn("Something wrong happened in schedule. This means the function went all the way through the switch without triggering any conditions");
+            break;
+      }
     }
   }
 }
 
-
-
-// this document is for validating the form and writing the results to the local storage
-function errorMessage() {
+window.addEventListener('load', function() {
   console.log("The TBA API is either offline or there is no available match schedule for the given event, so we need to manually input the match schedule");
-};
+});
 
-// function persistInput(input)
-// {
-//   var key = "input-" + input.id;
-//
-//   var storedValue = localStorage.getItem(key);
-//
-//   if (storedValue)
-//       input.value = storedValue;
-//
-//   input.addEventListener('input', function ()
-//   {
-//       localStorage.setItem(key, input.value);
-//   });
-// }
-
-// JSON objects will be stored in the local storage containing the inputted match schedule
-// One JSON object for each event
-// then the match no and the teams in order are put into an array
-// TODO: figure out how the actual TBA API serves up the JSON object containing the match data
-// Once we figure that out, we construct our JSON object to mimic that of the TBA API
-// for some reason Patrick was still using the v2 API
-
-// test JSON objects
+function changeButtonColor(id, color) {
+  button = document.getElementById(id);
+  button.style.backgroundColor = color;
+}
 
 testObj = {
             "eventname":"2018wapp",
@@ -58,20 +72,92 @@ testObj = {
             "blue3":[5803,69,6283,971]
           }
 
-//
-
-// let total = testObj.match.length;
 function testJSON() {
   var i;
   let schedule;
   console.log("event is: " + testObj.eventname);
   for (i=0; i <= (testObj.match.length-1); i++) {
     schedule = [testObj.match[i], testObj.red1[i], testObj.red2[i], testObj.red3[i], testObj.blue1[i], testObj.blue2[i], testObj.blue3[i]];
-
     console.log(schedule);
   }
 }
 
+// function to add rows to the table
+function addLocal() { // event nombre is a tribute to Patrick and his wonderful code
+  var i;
+  if(testObj) {
+    var eventNombre = testObj.eventname;
+    // matchno = document.createTextNode(testObj.match);
+    for(i=0; i < (testObj.match.length); i++) {
+      if (!document.getElementsByTagName) return;
+      // find the table
+      tabBody = document.getElementsByTagName("tbody").item(0);
+
+      // create a new row
+      row = document.createElement("tr");
+
+      // how many cells are included
+      cell1 = document.createElement("td");
+      cell2 = document.createElement("td");
+      cell3 = document.createElement("td");
+      cell4 = document.createElement("td");
+      cell5 = document.createElement("td");
+      cell6 = document.createElement("td");
+      cell7 = document.createElement("td");
+
+      // text nodes
+      matchno = document.createTextNode(testObj.match[i]);
+      red1 = document.createTextNode(testObj.red1[i]);
+      red2 = document.createTextNode(testObj.red2[i]);
+      red3 = document.createTextNode(testObj.red3[i]);
+      blue1 = document.createTextNode(testObj.blue1[i]);
+      blue2 = document.createTextNode(testObj.blue2[i]);
+      blue3 = document.createTextNode(testObj.blue3[i]);
+
+      // add text values to the cells
+      cell1.appendChild(matchno);
+      cell2.appendChild(red1);
+      cell3.appendChild(red2);
+      cell4.appendChild(red3);
+      cell5.appendChild(blue1);
+      cell6.appendChild(blue2);
+      cell7.appendChild(blue3);
+
+      // add td to try
+      row.appendChild(cell1);
+      row.appendChild(cell2);
+      row.appendChild(cell3);
+      row.appendChild(cell4);
+      row.appendChild(cell5);
+      row.appendChild(cell6);
+      row.appendChild(cell7);
+
+      // add tr to the table body
+      tabBody.appendChild(row);
+    }
+  } else {
+    alert('No schedule input yet');
+  }
+}
+
+function addTBA() {
+  var i;
+}
+
+
+
+
+
+
+
+
+
+
+// matchForm.html
+
+function recordInput() {
+    localStorage.setItem(document.getElementById());
+}
 
 // function that validates the submitted match schedule
 // it works using a checksum
@@ -87,8 +173,6 @@ function submitSchedule() {
   let blue2 = document.getElementById("blue2").value;
   let blue3 = document.getElementById("blue3").value;
 
-
-
   if(matchno >= 1 && matchno <= 200 && red2 <= 9999 && red2 >= 1 && red3 <= 9999 && red3 >= 1 && blue1 <= 9999 && blue1 >= 1 && blue2 <= 9999 && blue2 >= 1 && blue3 <= 9999 && blue3 >= 1) {
     checksum = checksum + 6;
   } if(checksum === conditions) {
@@ -96,90 +180,21 @@ function submitSchedule() {
   } else {
     return true;
   }
-  // switch (matchno >= 1 && matchno <= 200) {
-  //   case red1 <= 9999 && red1 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   case red2 <= 9999 && red2 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   case red3 <= 9999 && red3 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   case blue1 <= 9999 && blue1 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   case blue2 <= 9999 && blue2 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   case blue3 <= 9999 && blue3 >= 1:
-  //     checksum++;
-  //     console.log(checksum);
-  //
-  //   default:
-  //     return true;
-  // }
-
-// function to add rows to the table
-function addRows() { // event nombre is a tribute to Patrick and his wonderful code
-  var i = 0;
-  var eventNombre = testObj.eventname;
-  // matchno = document.createTextNode(testObj.match);
-  for(i=0; i < (testObj.match.length); i++) {
-    if (!document.getElementsByTagName) return;
-    // find the table
-    tabBody = document.getElementsByTagName("tbody").item(0);
-
-    // create a new row
-    row = document.createElement("tr");
-
-    // how many cells are included
-    cell1 = document.createElement("td");
-    cell2 = document.createElement("td");
-    cell3 = document.createElement("td");
-    cell4 = document.createElement("td");
-    cell5 = document.createElement("td");
-    cell6 = document.createElement("td");
-    cell7 = document.createElement("td");
-
-    // text nodes
-    matchno = document.createTextNode(testObj.match[i]);
-    red1 = document.createTextNode(testObj.red1[i]);
-    red2 = document.createTextNode(testObj.red2[i]);
-    red3 = document.createTextNode(testObj.red3[i]);
-    blue1 = document.createTextNode(testObj.blue1[i]);
-    blue2 = document.createTextNode(testObj.blue2[i]);
-    blue3 = document.createTextNode(testObj.blue3[i]);
-
-    // add text values to the cells
-    cell1.appendChild(matchno);
-    cell2.appendChild(red1);
-    cell3.appendChild(red2);
-    cell4.appendChild(red3);
-    cell5.appendChild(blue1);
-    cell6.appendChild(blue2);
-    cell7.appendChild(blue3);
-
-    // add td to try
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    row.appendChild(cell4);
-    row.appendChild(cell5);
-    row.appendChild(cell6);
-    row.appendChild(cell7);
-
-    // add tr to the table body
-    tabBody.appendChild(row);
-  }
-
-function recordInput() {
-    localStorage.setItem(document.getElementById());
-  }
 }
-}
+
+
+
+// function persistInput(input)
+// {
+//   var key = "input-" + input.id;
+//
+//   var storedValue = localStorage.getItem(key);
+//
+//   if (storedValue)
+//       input.value = storedValue;
+//
+//   input.addEventListener('input', function ()
+//   {
+//       localStorage.setItem(key, input.value);
+//   });
+// }
--- js/scoutMeetingTool.js
+++ js/scoutMeetingTool.js
@@ -11,22 +11,20 @@
 const TBAheader = "X-TBA-Auth-Key";
 const TBAkey = "d4V33bAbuXiKfuLW1pc4BaLbr56BgiORtyM5hwmRLU5qNf6Rxh83noDdI0mPJJ3R";
 const TBAURL = "https://www.thebluealliance.com/api/v3/event/";
-// const teamURL = "/teams/simple";
 const rankingURL = "/rankings";
 const matchesURL = "/matches/simple";
 
 // get data from TBA
-
 var rankings = new XMLHttpRequest();
-// var teams = new XMLHttpRequest();
 var matches = new XMLHttpRequest();
 
 // define JSON objects
 let matchesObj;
 let rankingsObj;
 
-// define the target URL
+let printedResults = false;
 
+// define the target URL
 function setTargetEvent() {
   // let target = this.value;
   if(document.getElementById("eventName").value === "ex: 2018wapp") {
@@ -39,120 +37,295 @@ function setTargetEvent() {
   }
 }
 
-// send the request now that the target has been determined
+// function that changes the submit button color for feedback
+function changeButtonColor(id, color) {
+  button = document.getElementById(id);
+  button.style.backgroundColor = color;
+}
 
+// send the request now that the target has been determined
 function sendRequest(target) {
   let cont = true;
+  let check1 = false;
+  let check2 = false;
+  let startbutton = document.getElementById("startbutton");
   if(setTargetEvent() === "STOP") {
     cont = false;
     return "target not set";
   } else {
-  rankings.open("GET", TBAURL.concat(setTargetEvent() + rankingURL));
-  rankings.setRequestHeader(TBAheader, TBAkey);
-  rankings.send();
-  rankings.onreadystatechange = function() {
-    if(this.readyState === 4) {
-    switch(this.status) {
-      case 200:
-        console.log("Ranking request completed successfully");
-        rankingsObj = JSON.parse(rankings.responseText);
-        break;
+    // turn the button yellow to show that the request is in progress
+    changeButtonColor("startbutton", "#fffa9b");
+    // send rankings http request
+    rankings.open("GET", TBAURL.concat(setTargetEvent() + rankingURL));
+    rankings.setRequestHeader(TBAheader, TBAkey);
+    rankings.send();
+    rankings.onreadystatechange = function() {
+      if(this.readyState === 4) {
+        switch(this.status) {
+            case 200:
+              console.log("Ranking request completed successfully");
+              rankingsObj = JSON.parse(rankings.responseText);
+              check1 = true;
+              changeButtonColor("startbutton", "#b7ffb4");
+              break;
 
-      case 401:
-        console.log("TBA API key is invalid");
-        console.log("Enter a valid key (R)");
-        alert("TBA API key is invalid; Enter a valid key por favor. Error 401 on rankings <-- for the tech support");
-        break;
+            case 401:
+              console.warn("TBA API key is invalid");
+              console.info("Enter a valid key (R)");
+              changeButtonColor("startbutton", "#ffb5b5");
+              alert("TBA API key is invalid; Enter a valid key por favor. Error 401 on rankings <-- for the tech support");
+              break;
 
-      case 404:
-        console.log("Invalid URL");
-        alert("Invalid URL entered. Error 404 on rankings <-- for the tech support");
-        break;
+            case 404:
+              console.info("Invalid URL");
+              changeButtonColor("startbutton", "#ffb5b5");
+              alert("Invalid URL entered. Error 404 on rankings <-- for the tech support");
+              break;
 
-      default:
-        console.log("Something wrong happened in rankings. This means the function went all the way through the switch without triggering any conditions");
-        break;
+            default:
+              changeButtonColor("startbutton", "#ffb5b5");
+              console.warn("Something wrong happened in rankings. This means the function went all the way through the switch without triggering any conditions");
+              break;
+        }
+      }
     }
+    // send matches http request
+    matches.open("GET", TBAURL.concat(setTargetEvent() + matchesURL));
+    matches.setRequestHeader(TBAheader, TBAkey);
+    matches.send();
+    matches.onreadystatechange = function() {
+      if(this.readyState === 4) {
+        switch (this.status) {
+          case 200:
+            console.log("Matches request completed successfully");
+            matchesObj = JSON.parse(matches.responseText);
+            check2 = true;
+            changeButtonColor("startbutton", "#b7ffb4");
+            break;
+
+          case 401:
+            console.warn("TBA key is invalid");
+            console.info("Enter a valid API key (M)");
+            changeButtonColor("startbutton", "#ffb5b5");
+            alert("TBA API key is invalid; Enter a valid key por favor. Error 401 on matches <-- for the tech support");
+            break;
+
+          case 404:
+            console.info("Invalid URL");
+            changeButtonColor("startbutton", "#ffb5b5");
+            alert("Invalid URL entered. Error 404 in matches <-- for the tech support");
+            break;
+
+          default:
+            changeButtonColor("startbutton", "#ffb5b5");
+            console.warn("Something wrong happened in matches");
+            break;
+        }
+      }
     }
-  }
-
-  matches.open("GET", TBAURL.concat(setTargetEvent() + matchesURL));
-  matches.setRequestHeader(TBAheader, TBAkey);
-  matches.send();
-  matches.onreadystatechange = function() {
-    if(this.readyState === 4) {
-    switch (this.status) {
-      case 200:
-        console.log("Matches request completed successfully");
-        matchesObj = JSON.parse(matches.responseText);
-        break;
-
-      case 401:
-        console.log("TBA key is invalid");
-        console.log("Enter a valid API key (M)");
-        alert("TBA API key is invalid; Enter a valid key por favor. Error 401 on matches <-- for the tech support");
-        break;
-
-      case 404:
-        console.log("Invalid URL");
-        alert("Invalid URL entered. Error 404 in matches <-- for the tech support");
-        break;
-
-      default:
-        console.log("Something wrong happened in matches");
-        break;
-    }
-    }
-  }
   }
 }
 
 // sort the teams once they have been recieved, and match the team numbers and rankings
-
 function sortMatches() {
-  matchesObj.match_number.sort(function(a, b){return a - b})
+  // matchesObj.match_number..sort(function(a, b){return a - b})
 }
 
-// we don't need to sort the rankings because they are given to us in order
-// function sortRankings() {
-//   rankingsObj.rankings.team_key.sort(function(a, b){return a - b});
-// }
-
 // function that creates the text input box for the match predictions
-
 function addTextBox() {
-  var textBox = document.createElement("input")
-  this.appendChild(textBox);
-  this.setAttribute(required, "");
-  this.setAttribute(type, "string");
-  // this.setAttribute(class, "t");
-  // this.setAttribute();
+
 }
 
 // determine how many matches each team has left
-
 function matchesLeft() { // use the .insertBefore(insert, between); method to insert the new columns in the right place
 
 }
 
 // add values to table
-
 function addTeams() {
+  removePlayoffs();
+  var i;
+  for(i = 0; i < rankingsObj.rankings.length; i++) {
+    team = parseInt(rankingsObj.rankings[i].team_key.substr(3), 10);
+    points = rankingsObj.rankings[i].extra_stats[0];
+    rank = rankingsObj.rankings[i];
+    standing = i + 1;
+    console.log(team + " is rank " + standing + " with " + points + " ranking points!");
 
+    if(!document.getElementsByTagName) return;
+
+    // now we are finding the table
+    table = document.getElementsByTagName("tbody").item(0);
+
+    // create a new row
+    row = document.createElement("tr");
+    row.setAttribute("id", team);
+
+    // create some cells
+    standingcell = document.createElement("td");
+    teamnocell = document.createElement("td");
+    currentrankingscell = document.createElement("td");
+    totalrankingscell = document.createElement("td");
+    teamstandingcell = document.createElement("td");
+
+    // text nodes for each
+    teamno = document.createTextNode(team);
+    currentrankings = document.createTextNode(points);
+    totalrankings = document.createTextNode("");
+    teamstanding = document.createTextNode("");
+
+    // add the text nodes to the cells
+    standingcell.appendChild(document.createTextNode(standing))
+    teamnocell.appendChild(teamno);
+    currentrankingscell.appendChild(currentrankings);
+    totalrankingscell.appendChild(totalrankings);
+    teamstandingcell.appendChild(teamstanding);
+
+    // append the cells to the row
+    row.appendChild(standingcell);
+    row.appendChild(teamnocell);
+    row.appendChild(currentrankingscell);
+    row.appendChild(totalrankingscell);
+    row.appendChild(teamstandingcell);
+
+    // append the row to the table
+    table.appendChild(row);
+  }
+  printedResults = true;
+  return "Done";
 }
 
-// add up all of the predictions and
+// reset the Page
+function clearResults() {
+  if(printedResults) {
+    if(confirm("Do you really want to clear the results?")) {
+      document.location.reload(false);
+    }
+  }
+}
 
+// add up all of the predictions
 function aggregatePoints() {
 
 }
 
-// isolate the necessary parts: team numbers, ranking points,
+// this function removes any playoff matches
+function removePlayoffs() {
+  matchesObj = matchesObj.filter(result => result.comp_level === "qm");
+  matchesObj = matchesObj.sort((a, b) => a.match_number - b.match_number);
+}
+
+function showComplete() {
+  removePlayoffs();
+  var i;
+  var j;
+  var k;
+  for(i = 0; i < matchesObj.length; i++) {
+    // red team
+    red1 = parseInt(matchesObj[i].alliances.red.team_keys[0].substr(3), 10);
+    red2 = parseInt(matchesObj[i].alliances.red.team_keys[1].substr(3), 10);
+    red3 = parseInt(matchesObj[i].alliances.red.team_keys[2].substr(3), 10);
+
+    // blue team
+    blue1 = parseInt(matchesObj[i].alliances.blue.team_keys[0].substr(3), 10);
+    blue2 = parseInt(matchesObj[i].alliances.blue.team_keys[1].substr(3), 10);
+    blue3 = parseInt(matchesObj[i].alliances.blue.team_keys[2].substr(3), 10);
+
+    if(!document.getElementsByTagName) return;
+
+    // find the table
+    table = document.getElementsByTagName("tbody").item(1);
+
+    // create a new row
+    row = document.createElement("tr");
+
+    // create the cells
+    cellmn = document.createElement("td");
+    cellr1 = document.createElement("td");
+    cellr2 = document.createElement("td");
+    cellr3 = document.createElement("td");
+    cellb1 = document.createElement("td");
+    cellb2 = document.createElement("td");
+    cellb3 = document.createElement("td");
+    rpr = document.createElement("td");
+    rpb = document.createElement("td");
+
+    // create the text that will go inside
+    match_no = document.createTextNode(matchesObj[i].match_number);
+    cellr1_t = document.createTextNode(red1);
+    cellr2_t = document.createTextNode(red2);
+    cellr3_t = document.createTextNode(red3);
+    cellb1_t = document.createTextNode(blue1);
+    cellb2_t = document.createTextNode(blue2);
+    cellb3_t = document.createTextNode(blue3);
+    rpr_t = document.createElement("input");
+    rpb_t = document.createElement("input");
+
+    // give each team its own class
+    cellr1.setAttribute("class", red1);
+    cellr2.setAttribute("class", red2);
+    cellr3.setAttribute("class", red3);
+    cellb1.setAttribute("class", blue1);
+    cellb2.setAttribute("class", blue2);
+    cellb3.setAttribute("class", blue3);
+
+    // give the inputs their own attributes so we can find them and add them ups
+    rpr_t.setAttribute("type", "number");
+    rpr_t.setAttribute("required", "");
+    rpr_t.setAttribute("max", 4);
+    rpr_t.setAttribute("min", 0);
+    rpr_t.setAttribute("class", "redform");
+    rpr_t.setAttribute("name", "Red Input Box");
+    rpb_t.setAttribute("type", "number");
+    rpb_t.setAttribute("required", "");
+    rpb_t.setAttribute("max", 4);
+    rpb_t.setAttribute("min", 0);
+    rpb_t.setAttribute("class", "blueform");
+    rpb_t.setAttribute("name", "Blue Input Box");
+
+    // add the text nodes to the cells
+    cellmn.appendChild(match_no);
+    cellr1.appendChild(cellr1_t);
+    cellr2.appendChild(cellr2_t);
+    cellr3.appendChild(cellr3_t);
+    cellb1.appendChild(cellb1_t);
+    cellb2.appendChild(cellb2_t);
+    cellb3.appendChild(cellb3_t);
+    rpr.appendChild(rpr_t);
+    rpb.appendChild(rpb_t);
+
+    // add the cells to the row
+    row.appendChild(cellmn);
+    row.appendChild(cellr1);
+    row.appendChild(cellr2);
+    row.appendChild(cellr3);
+    row.appendChild(cellb1);
+    row.appendChild(cellb2);
+    row.appendChild(cellb3);
+    row.appendChild(rpr);
+    row.appendChild(rpb);
+
+    // add the row to the table
+    table.appendChild(row);
+  }
+  printedResults = true;
+  return "Done";
+}
 
 window.addEventListener('load', function() {
   console.log("loaded up");
 });
 
-// var run = function() { // first find target url, then get http, then find matches remaining, total
-//   sendRequest(setTargetEvent());
+// When the user scrolls the page, execute stickHeader
+// window.onscroll = function() {stickyHeader()};
+//
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyHeader() {
+//   var header = document.getElementById("stickyHeader");
+//   var sticky = header.offsetTop;
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
 // }
