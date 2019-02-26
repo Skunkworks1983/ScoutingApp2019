dee6d82d56d4c6713269a152fe199d0cdcff5ed0
Unknown <efelix2k3@gmail.com>
2019-02-25 19:46:12 -0800

    schema

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Public.DeepSpace(
+isTele                    bool, -- Is it tele?
+scoutName                 varchar(25),
+eventName                 varchar(20),
+teamNo                    int,
+match                     int,
+alliance                  varchar(4),
+autoStartLevel            int,
+autoCrossLine             bool,
+autoRocketObsured         int,
+autoCargoObsured          int,
+autoRocketUnobsured       int,
+autoCargoUnobsured        int,
+NoShow                    bool,
+teleRocketObscured        int,
+teleCargoObscured         int,
+teleRocketUnobscured      int,
+teleCargoUnobscured       int,
+deadBot                   bool,
+climbLevel                int,
+assistedClimb             int,
+recievedClimb             bool,
+PRIMARY KEY	(eventname, teamno, match));
